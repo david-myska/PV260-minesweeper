54aefab53171d9b545f2d1da782762d2581c5ab5
standySotak <xsotak@fi.muni.cz>
2021-03-20 14:18:42 +0100

    GameBoardGetPositionTest added

--- Minesweeper/MinesweeperTests/GameBoardTests.cs
+++ Minesweeper/MinesweeperTests/GameBoardTests.cs
@@ -12,14 +12,44 @@ namespace MinesweeperTests
         }
 
         [Test]
-        public void GivenBoardWithMine_ThenGetReturnsMine()
+        [TestCase(Field.EmptyCovered)]
+        [TestCase(Field.EmptyRevealed)]
+        [TestCase(Field.Flagged)]
+        [TestCase(Field.Mine)]
+        public void GivenBoardWithMine_ThenGetReturnsMine(Field field)
         {
             IGameBoardCreator gameBoardCreator = A.Fake<IGameBoardCreator>();
-            A.CallTo(() => gameBoardCreator.GenerateGameBoard(1, 1)).Returns(new Field[1, 1] { { Field.Mine } });
+            A.CallTo(() => gameBoardCreator.GenerateGameBoard(1, 1)).Returns(new Field[1, 1] { { field } });
 
             IGameBoard gameBoard = new GameBoard(gameBoardCreator, 1, 1);
 
-            Assert.That(gameBoard.Get(1, 1) == Field.Mine);
+            Assert.That(gameBoard.Get(1, 1) == field);
+        }
+
+        [Test]
+        [TestCase(1, 1)]
+        [TestCase(1, 2)]
+        [TestCase(1, 3)]
+        [TestCase(2, 1)]
+        [TestCase(2, 2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 1)]
+        [TestCase(3, 2)]
+        [TestCase(3, 3)]
+        public void GivenNonTrivialBoard_GetReturnsCorrectPosition(int posX, int posY)
+        {
+            IGameBoardCreator gameBoardCreator = A.Fake<IGameBoardCreator>();
+            A.CallTo(() => gameBoardCreator.GenerateGameBoard(3, 3)).Returns(new Field[3, 3] {                 
+                { Field.EmptyCovered, Field.EmptyRevealed, Field.Flagged },
+                { Field.Flagged, Field.EmptyRevealed, Field.Mine},
+                { Field.Mine, Field.EmptyCovered, Field.Mine}
+            });
+
+            Field[,] generatedBoard = gameBoardCreator.GenerateGameBoard(3, 3);
+
+            IGameBoard gameBoard = new GameBoard(gameBoardCreator, 3, 3);
+
+            Assert.That(gameBoard.Get(posX, posY) == generatedBoard[posX - 1, posY - 1]);
         }
     }
 }
